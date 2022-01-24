725dd18f6971556aae623841f8142030c9c1b589
guntakasivar <guntakasivar@gmail.com>
2022-01-24 11:45:33 +0530

    adding next line

--- Ex1/src/Test1.java
+++ Ex1/src/Test1.java
@@ -3,5 +3,6 @@ import java.security.DomainCombiner;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("welcome");
+                System.out.println("bye...");
 	}
 }
